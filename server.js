d32783e5f95f09e406dcf0e46d2eff82785d2e7d
Mahir Erden <mahirerden@gmail.com>
2020-03-17 12:33:27 -0400

    Database connection added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,9 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password123@ds013232.mlab.com:13232/heroku_wndkz3vh", {
+    useNewUrlParser: true,
+    useFindAndModify: false
 });
 
 // routes
